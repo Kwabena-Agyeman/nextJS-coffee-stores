d90c18bdffbb64b513a8d1bda2712a8c29a9f0b2
Kwabena Agyeman <kagyeman@ymail.com>
2022-01-04 14:40:32 +0000

    added background image

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -2,12 +2,23 @@
 
 import React from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const CoffeStore = () => {
   const router = useRouter();
-
   console.log("router", router);
-  return <div>{router.query.id}</div>;
+
+  return (
+    <div>
+      Coffee Store - {router.query.id}{" "}
+      <Link href={"/"}>
+        <a>BacK to home</a>
+      </Link>
+      <Link href={"/coffee-store/koby"}>
+        <a>Go to store</a>
+      </Link>
+    </div>
+  );
 };
 
 export default CoffeStore;
