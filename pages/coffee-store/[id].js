0ccf1fce53b65f39d7b7ec24f1c7388c2c11b23d
Kwabena Agyeman <kagyeman@ymail.com>
2022-01-05 13:17:32 +0000

    set up state props

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -3,12 +3,20 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Head from "next/head";
 
 import coffeeStoreDate from "../../data/coffee-stores.json";
 
 export function getStaticPaths() {
+  const paths = coffeeStoreDate.map((CS) => {
+    return {
+      params: {
+        id: CS.id.toString(),
+      },
+    };
+  });
   return {
-    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    paths: paths,
     fallback: false,
   };
 }
@@ -17,18 +25,21 @@ export function getStaticProps(context) {
   console.log("params", context.params);
   return {
     props: {
-      coffeStore: coffeeStoreDate.find((coffeeStore) => {
+      coffeeStore: coffeeStoreDate.find((coffeeStore) => {
         return coffeeStore.id === parseInt(context.params.id);
       }),
     },
   };
 }
 
-const CoffeStore = () => {
+const CoffeStore = ({ coffeeStore }) => {
   const router = useRouter();
 
   return (
     <div>
+      <Head>
+        <title>{coffeeStore.name}</title>
+      </Head>
       Coffee Store - {router.query.id}{" "}
       <Link href={"/"}>
         <a>BacK to home</a>
@@ -36,6 +47,9 @@ const CoffeStore = () => {
       <Link href={"/coffee-store/koby"}>
         <a>Go to store</a>
       </Link>
+      <p>{coffeeStore.address}</p>
+      <p>{coffeeStore.name}</p>
+      <p>{coffeeStore.neighbourhood}</p>
     </div>
   );
 };
